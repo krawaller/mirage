3e581469350d38396800ac358e006c1352f0a657
KungD <david@krawaller.se>
2012-05-11 11:51:20 +0200

    docco-generated docs

--- mirage.js
+++ mirage.js
@@ -1,7 +1,21 @@
 this.Mirage = (function(){
-	
+
+// ### Property views
+// These views are used to display a single model property. There are 3 different flavours of display:
+//
+//*   *label*: will show the name of the property
+//*   *value*: will show the value of the property
+//*   *edit*: will show a control to edit the value of the property
+//
+// Each property type will have its own view (text,boolean,selectlist,etc), and each such type-specific
+// view will be able to render the property in those three flavours.
+
+// #### Property base view
+// An abstract baseclass, inherited by all Property views
 	var PropertyBaseView = Backbone.View.extend({
 		tagName: "span",
+		// Called from initialize with viewkind (label/value/edit) as argument.
+		// Will call &lt;viewkind&gt;Html to create content, and pass along to elementWrapper
 		buildElement: function(viewkind){
 			var o = this.propdef,
 				val = this.model.attributes[o.name],
@@ -11,24 +25,39 @@ this.Mirage = (function(){
 			}
 			return this.elementWrapper(viewkind,o.type,content);
 		},
+		// Used in buildElement. Responsible for wrapping content in span (if needed),
+		// and adding relevant css classes.
 		elementWrapper: function(viewkind,proptype,content){
 			var $c = $(content),
 				$el = $c.length == 1 && viewkind !== "value" ? $c : $("<span>"+content+"</span>");
 			return $el.addClass("prop-"+viewkind).addClass("prop-"+proptype+"-"+viewkind);
 		},
+		// The callback used to update a value propview when the model attribute changes.
+		// This default implementation will simply repopulate the element with a new
+		// call to valueHtml.
 		updateValueElement: function(){
 			this.$el.html(this.valueHtml(this.propdef,this.model.attributes[this.propdef.name]));
 		},
-		labelHtml: function(o){
-			return o.label||o.name;
+		// Default implementation of generating content for a label element. Will simply use the name
+		// of the property, or the 'label' property of the propdef if one is provided.
+		labelHtml: function(propdef){
+			return propdef.label||propdef.name;
 		},
+		// Default implementation of generating content for a value element. Will simply pass along
+		// the current value of the relevant model attribute, which is supplied as the second 
+		// argument to all html generator functions.
 		valueHtml: function(o,val){
 			return val;
 		},
+		// Returns the current value of the edit element control. Used in the Mirage ModelView to 
+		// collect all entered values upon form submission.
 		getValue: function(){
 			return this.$("input.prop-edit-ctrl").val();
 		},
-		initialize: function(opts){ // must have a propdef
+		// The initialize function needs a propdef (Property definition) and a kind (one of label,
+		// value or edit). It will store the propdef on the instance for later access, and build
+		// the correct element depending on the kind.
+		initialize: function(opts){
 			this.propdef = opts.propdef;
 			this.setElement(this.buildElement(opts.kind));
 			this.$el.addClass("prop-"+opts.propdef.type);
